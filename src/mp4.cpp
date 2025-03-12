3ee4816505b371e885e3857d0b87da649704e354
Rien88 <erin@robotics88.com>
2025-03-11 21:17:00 -0400

    Added publishing camera info from calibration yaml

--- src/mp4.cpp
+++ src/mp4.cpp
@@ -1,8 +1,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
+#include <camera_info_manager/camera_info_manager.hpp>
 #include <sstream>
 
 int main(int argc, char * argv[])
@@ -10,15 +12,17 @@ int main(int argc, char * argv[])
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("mp4_to_ros2");
 
-    std::string video_file, frame_id;
+    std::string video_file, frame_id, camera_info_file;
     bool bag_sync = true;
     node->declare_parameter("video_file", video_file);
     node->declare_parameter("frame_id", frame_id);
     node->declare_parameter("bag_sync", bag_sync);
+    node->declare_parameter("camera_info_file", camera_info_file);
 
     node->get_parameter("video_file", video_file);
     node->get_parameter("frame_id", frame_id);
     node->get_parameter("bag_sync", bag_sync);
+    node->get_parameter("camera_info_file", camera_info_file);
 
     if (bag_sync) {
         RCLCPP_INFO(node->get_logger(), "Waiting for IMU message to start video publishing");
@@ -38,8 +42,10 @@ int main(int argc, char * argv[])
         imu_sub.reset();
     }
 
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher =
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher =
         node->create_publisher<sensor_msgs::msg::Image>("/mp4/video_frames", 10);
+    rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_publisher =
+        node->create_publisher<sensor_msgs::msg::CameraInfo>("/mp4/camera_info", 10);
 
     cv::VideoCapture cap(video_file); 
     if (!cap.isOpened())
@@ -69,8 +75,13 @@ int main(int argc, char * argv[])
         std::cout << "starting time was " << start_time.seconds() << " sec" << std::endl;
     }
 
+    // Load camera info
+    std::string camera_info_url = "file://" + camera_info_file;
+    camera_info_manager::CameraInfoManager cam_info_manager(node.get(), "mp4", camera_info_url);
+    sensor_msgs::msg::CameraInfo::SharedPtr camera_info_msg = std::make_shared<sensor_msgs::msg::CameraInfo>(cam_info_manager.getCameraInfo());
+
     cv::Mat frame;
-    sensor_msgs::msg::Image::SharedPtr msg;
+    sensor_msgs::msg::Image::SharedPtr image_msg;
     rclcpp::WallRate loop_rate(fps); // Adjust the rate as needed
 
     while (rclcpp::ok() && cap.read(frame))
@@ -85,16 +96,18 @@ int main(int argc, char * argv[])
             int sec = floor(timestamp_seconds);
             rclcpp::Duration t(sec, nsec);
             header.stamp = start_time + t;
-            msg = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
+            image_msg = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
+            camera_info_msg->header = header;
         } 
         catch (cv_bridge::Exception& e) 
         {
             RCLCPP_ERROR(node->get_logger(), "cv_bridge exception: %s", e.what());
             return 1;
         }
-        frame_count ++;
+        frame_count++;
 
-        publisher->publish(*msg);
+        image_publisher->publish(*image_msg);
+        camera_info_publisher->publish(*camera_info_msg);
         rclcpp::spin_some(node);
         loop_rate.sleep();
     }
